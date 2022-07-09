669535bf09d12363d9c6163abbc1bf5b5e3cde51
balazscsintalan <balazscsintalan@users.noreply.github.com>
2022-07-09 21:32:13 +0200

    adding prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,7 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div>pokemon</div>
-  )
-}
+  return <div>pokemon</div>;
+};
 
-export default Home
+export default Home;
